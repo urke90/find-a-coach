a8680b9e7a8c26c476f163d630d7128bcee57e77
urke90 <urosbijelic90@gmail.com>
2021-10-07 13:45:20 +0200

    added vuex and getters for coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,13 +8,27 @@
                 <button>Refresh</button>
                 <router-link to="register">Register as Coach</router-link>
             </div>
-            <ul>
-                List of coaches
+            <ul v-if="hasCoaches">
+                <li v-for="coach in coaches" :key="coach.id">
+                    {{ coach.firstName }}
+                </li>
             </ul>
+            <h3 v-else>No Coaches Found</h3>
         </section>
     </div>
 </template>
 
-<script></script>
+<script>
+export default {
+    computed: {
+        coaches() {
+            return this.$store.getters.coaches;
+        },
+        hasCoaches() {
+            return this.$store.getters.hasCoaches;
+        }
+    }
+};
+</script>
 
 <style scoped></style>
---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import coaches from './coaches/coaches';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {
+        coaches
+    }
+});
