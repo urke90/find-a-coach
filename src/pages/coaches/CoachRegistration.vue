4816894b5bb9a86bfe2136c50ffba44c157ead93
urke90 <urosbijelic90@gmail.com>
2021-10-09 16:38:37 +0200

    created registration form component

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        <h2>Coache Registration</h2>
-    </div>
+    <section>
+        <base-card>
+            <coach-form @submit-data="registerCoach"></coach-form>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+    components: {
+        CoachForm
+    },
+    methods: {
+        registerCoach(data) {
+            console.log('data iz register coach forme', data);
+        }
+    }
+};
+</script>
